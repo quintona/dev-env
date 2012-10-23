db4c80a7d7df260b0367204b701956d0b0143895
Quinton Anderson <quintona@gmail.com>
2012-10-23 12:42:11 +0200

    Added the MRS project

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,6 +5,12 @@ trac::project {"Izazi":
 } -> agilo::project {"Izazi": require => trac::project['Izazi'], } -> class {'gitblit': }
 
 
+trac::project {"MRS":
+	repository_path	=>	"/home/vagrant/MRS/.git",
+	description	=>  "Medical Rebate System",
+	config => "defaults",
+} -> agilo::project {"MRS": require => trac::project['MRS'], }
+
 # Improvemens: 
 # - LDAP Integration
 # - Single sign on
